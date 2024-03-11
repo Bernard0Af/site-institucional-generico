3dc86509f5308363f45802f55c15e7aa5b262494
Bernardo A <araujobernardo11111@gmail.com>
2024-03-11 14:06:40 -0300

    Tarefa 4 concluída

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,15 @@
 const button = document.querySelector(".header__button")
+const buttonSecund = document.querySelector(".article_button")
 const modal = document.querySelector(".modal__email")
 const buttonClose = document.querySelector(".modal__button__close")
 
 
 button.addEventListener("click", () =>{
     modal.showModal();
+})
+
+buttonSecund.addEventListener("click", () => {
+    modal.showModal()
 })
 
 buttonClose.addEventListener("click", () => {
